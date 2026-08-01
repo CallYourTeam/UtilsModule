4a5509bbeb093e2bfb6638ba8799686ada816d0f
d1orn <zhinovnikita@yandex.ru>
2026-08-02 01:49:02 +0700

    init

---
+++ Utils.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace UtilsModule
+{
+    public static class Utils
+    {
+        public static string ListToString<T>(List<T> list)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var item in list)
+            {
+                sb.Append(item);
+                sb.Append(';');
+            }
+
+            return sb.ToString();
+        }
+
+        public static List<T> StringToList<T>(string str, Func<string, T> strToItemFunc)
+        {
+            var list = new List<T>();
+
+            var strList = str.Split(';');
+
+            foreach (var item in strList)
+            {
+                list.Add(strToItemFunc(item));
+            }
+
+            return list;
+        }
+    }
+}
